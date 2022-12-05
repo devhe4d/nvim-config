95eebf8556f5f1d9d59d3ba24dd9a70c80c52760
Shahryar <shahryar.rajabpoor@gmail.com>
2022-12-05 12:51:15 +0100

    lsp added

--- lua/devhead/core/keymaps.lua
+++ lua/devhead/core/keymaps.lua
@@ -27,3 +27,11 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
